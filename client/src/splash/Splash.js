73bf2c1ed601ca596e85450863f2daf496a9fcd5
Matthew Shang <mshang47@gmail.com>
2019-06-11 10:33:04 -0700

    Started Splash screen

---
+++ client/src/splash/Splash.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../App.css'
+
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    alert(`Submitted ${this.state.username}`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Trivia Royale</h1>
+        <p>What would you like to be called?</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleChange} />
+          <input type="submit" value="Enter" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Splash;
